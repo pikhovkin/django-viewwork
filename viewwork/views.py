833cc265bef7cde4afa5b0695d1238cf9b599080
Sergey Pikhovkin <pikhovkin@gmail.com>
2020-12-02 21:59:55 +0300

    Add NoReverseMatch for reverse url

--- viewwork/views.py
+++ viewwork/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import View
 from django.http import JsonResponse
-from django.urls import reverse
+from django.urls import reverse, NoReverseMatch
 
 from .models import Menu
 from .utils import build_nested_tree
@@ -20,12 +20,15 @@ class MenuView(View):
             'id', 'parent_id', 'name', 'view'
         ).order_by('parent_id', 'sort_order', 'name')
 
-    def get(self, request):
+    def get(self, request, *args, **kwargs):
         menu_items = self.get_queryset()
         data = []
         for item in menu_items:
             if item['view']:
-                item['url'] = reverse(item['view'])
+                try:
+                    item['url'] = reverse(item['view'])
+                except NoReverseMatch:
+                    continue
             data.append(item)
         tree = build_nested_tree(data)
         return JsonResponse(tree, safe=False)
