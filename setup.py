db688dac60ff5dfa00b2a94de4670d0662c793a6
Sergei Pikhovkin <pikhovkin@gmail.com>
2021-10-27 18:46:56 +0300

    Update requirements

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ require_all = requirements + require_select2
 
 setup(
     name='django-viewwork',
-    version='0.3.1',
+    version='0.4.0',
     description='Collect views, generate urls and create menu',
     long_description=readme,
     long_description_content_type='text/markdown',
